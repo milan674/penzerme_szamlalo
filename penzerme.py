1fe05888eb814151826746b0d6871e8e00080733
milan674 <45066705+milan674@users.noreply.github.com>
2020-11-18 10:37:35 +0100

    Update penzerme.py

--- penzerme.py
+++ penzerme.py
@@ -2,47 +2,113 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-# kep beolvasasa
-img = cv2.imread('1.jpg')
+#1.rész: kép átméretezés, szürkeárnyalatossá alakítás,Gauss szűrő alkalmazása, körök megkeresése a képen
 
-# kep atmeretezese
-(h, w) = img.shape[:2]   # eredeti kep magassaganak es szelessegenek lekerdezese 
-print ("Az eredeti kep merete: ", (h,w)) 
-new_width=800  # megadom, hogy mekkora legyen az uj szelesseg :  Ebbol kiszamolja  a magassagot, igy az aranyok nem valtoznak
-ratio = new_width /w  # az uj es regi szelesseg aranya ---> ezzel kell beszorozni a regi magassagot, es megkapom az uj magassagot
-dim = (new_width,int(h * ratio) ) # uj magassag, uj meret kiszamitasa     dim -> tuple = (uj_szelesseg,uj_magassag)
-resized = cv2.resize(img, dim, cv2.INTER_AREA)  # atmeretezes
-print ("Az atmeretezett kep merete: ", dim )
+def Resize(img):            # kép átméretezése
+    (h,w) = img.shape[:2]   #eredeti kép magasságának és szélességének lekérdezése
+    print ("Az eredeti kep merete: ", (h,w)) 
+    new_width=1440  # megadom, hogy mekkora legyen az új szélesseg :  Ebből kiszámolja  a magasságot, így az arányok nem változnak
+    ratio = new_width /w  # az új és régi szélesseg aránya ---> ezzel kell beszorozni a régi magasságot, es megkapom az új magasságot
+    dim = (new_width,int(h * ratio) ) # új magasság, új méret kiszámítása     dim -> tuple = (uj_szelesseg,uj_magassag)
+    resized = cv2.resize(img, dim, cv2.INTER_AREA)  # átméretezés
+    print ("Az atmeretezett kep merete: ", resized.shape[:2] )
+    return resized
 
-output=resized.copy()  # Masolat az atmeretezett szines keprol, kesobb erre irom ki a talalatokat
+def FindCircles(gray_img,radius): #körök megkeresése : HoughCircles segítségével
+    circles = cv2.HoughCircles(gray_img,cv2.HOUGH_GRADIENT,1,120,param1=40,param2=25, minRadius=40, maxRadius=170)
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        radius.append(i[2]) # a körök sugarait tároló tömbhöz hozzáfűzi az aktuális kör sugarának az értékét
+    print("     X    Y     R" )
+    print(circles) # megtalált körök középpontjának x és y koordinátái, és sugara
+    print(" Megtalaltam", circles.shape[1],  "db penzermet!")
+    return circles
 
-#szurkearnyalatossa alakitas
-gray_img = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY) 
-# Gauss szűrő alkalmazasa, a reszleteket elrejti
-gray_img= cv2.GaussianBlur(gray_img,(11,11),cv2.BORDER_DEFAULT)
-
-cv2.imshow("g",gray_img)
-
-#KOROK MEGKERESESE : HoughCircles fgv 
-korok = cv2.HoughCircles(gray_img,cv2.HOUGH_GRADIENT,1,120,param1=40,param2=35, minRadius=50, maxRadius=200)
-print(korok) # megtalalt korok x,y koord és r sugar
-#print(korok.shape) # a masodik tagja az, hogy mennyi kort talalt
-print(" Megtalaltam", korok.shape[1],  "db penzermet!")
-
-# megtalalt korok megrajzolasa,  es egyelore kiirja a kepre, hogy melyik ermet hanyadiknak tarolta el a tombben
-szamlalo = 1
-for i in korok[0, :]:
-    cv2.circle(output, (i[0], i[1]), int(i[2]), (0, 255, 0), 5) # rajzol egy kort az ermek kore
-    cv2.circle(output, (i[0], i[1]), 2, (255, 255, 0), 5) # megrajozlja a kor kozeppontjat
-    cv2.putText(output, str(szamlalo), (int(i[0]) - 70, int(i[1]) + 30), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0),5) # kiirja a kepre, hogy melyik ermet hanyadik helyen talalta
-    szamlalo+=1
- 
-# 2. resz: megtalalt korok elemzese: milyen intenzitasu, mekkora a sugara--> milyen erme? 
+# 2. rész: megtalált körök elemzése: milyen intenzitású, mekkora a sugara?--> milyen érme? 
+def averageIntensity(img, circles):
+    av_values = []
+    for coordinates in circles[0,:]:
+        r=coordinates[2]
+        """
+        r=80 #próbálkozás kisebb értékkel, a pontosabb eredmény miatt
+        coin= img[coordinates[1] - r:coordinates[1] + r, coordinates[0] - r:coordinates[0] + r] 
+        plt.imshow(coin)  #érmék kivágott területének megjelenítése
+        plt.colorbar()  
+        plt.show() 
+        """
+        column = np.mean(img[coordinates[1]-r:coordinates[1]+r, coordinates[0]-r:coordinates[0]+r]) 
+        av_values.append(np.around(column))
+    print (av_values)
+    return av_values
 
 
+def CoinAnalysis(radius,colour_values,Found_coins,output):  # melyik körben milyen típusú érme van?
+    sum=0 #pénzérmék összegét tárolja
+    count=0
+    for i,j in zip (radius,colour_values):
+        if  i < 115 :
+                    print("5 Ft-os erme")
+                    sum+=5
+                    Found_coins.append(5)
+        elif (i >= 120 and i<=135) and ( j>115 ): 
+                    print ("10 ft-os erme")
+                    sum+=10
+                    Found_coins.append(10)
+        elif(i >= 125 and i <= 135) and (j>=90 and j<=107):
+                    print ("20 ft-os erme")
+                    sum+=20
+                    Found_coins.append(20)
+        elif (i >= 135 and i <=147) and (j>=100):
+                    print ("50 ft-os erme")
+                    sum+=50
+                    Found_coins.append(50)
+        elif (i >= 110 and i <= 132) and j<90:
+                    print ("100 ft-os erme")
+                    sum+=100
+                    Found_coins.append(100)
+        elif (i >= 140) and j<100:
+                    print ("200 ft-os erme")
+                    sum+=200
+                    Found_coins.append(200)
 
-plt.imshow(output, cmap="gray")  # megjelenites szurkearnyalatosan
-plt.colorbar()  # intenzitas mutato
+    for k in circles[0,:]:  #pénzérmék típusainak kiirása a képre
+         cv2.putText(output, str(Found_coins[count]),(k[0]-60,k[1]),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,0,0), 5)
+         count += 1
+    cv2.putText(output,"Az ermek osszege: "+ str(sum)+ " Ft",(10,1000),cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0),2)
+    return sum
+
+def DrawCircles(image,circles):   # megtalált körök megrajzolása és kiírja a képre, hogy melyik érmét hányadiknak tárolta el
+    szamlalo = 1
+    for i in circles[0, :]:
+        cv2.circle(image, (i[0], i[1]), (i[2]), (0, 255, 0), 5) # rajzol egy kört az érmék köré
+        #cv2.circle(image, (i[0], i[1]), 2, (255, 255, 0), 5) # megrajozlja a kör középpontját
+        # kiírja a képre, hogy melyik érmét hányadik helyen találta
+        #cv2.putText(image, str(szamlalo), ((i[0]) - 70, (i[1]) + 30), cv2.FONT_HERSHEY_SIMPLEX,1.5, (255, 0, 0),5) 
+        szamlalo+=1
+    return image
+
+#-------------MAIN----------
+#1.rész: kép átméretezés, szürkeárnyalatossá alakítás,Gauss szűrő alkalmazása, körök megkeresése a képen
+img = cv2.imread('58.jpg') # kép beolvasása
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+resized=Resize(img)  #kép átméretezése
+output=resized.copy()  # Másolat az átméretezett színes képről, később erre írom ki a találatokat
+
+gray_img = cv2.cvtColor(resized.astype(np.uint8), cv2.COLOR_BGR2GRAY)   #szürkeárnyalatossá alakítás
+blurred_img=cv2.GaussianBlur(gray_img,(31,31),0)# Gauss szűrő alkalmazása, a részleteket elrejti, azokra a körök keresésénel nincs szükség
+cv2.imwrite("blurred.jpg",blurred_img)# ellenőrzés, hogy a  Gauss szűrő sikeres?
+cv2.imwrite("szurke.jpg",gray_img) # ellenőrzés, hogy a szürkeárnyalatos átalakítás sikeres?
+
+radius=[]  # ebben tárolom el a körök sugarát
+circles=FindCircles(blurred_img,radius)  # körök megkeresése, kör középpontjának x,y koordinátáinak eltárolása, kör sugarának eltárolása
+
+# 2. rész: megtalált körök elemzése: milyen intenzitású, mekkora a sugara?--> milyen érme?   kimeneti kép elkészítése
+Found_coins = []   # ebben tárolja el, hogy milyen értékű érméket talált
+colour_values = averageIntensity(output, circles)   # talált körök (érmék) átlagos színintenzitások eltárolása
+sum=CoinAnalysis(radius,colour_values,Found_coins,output) # Eldönti, hogy a talált érmék milyen típusúak és mennyi az összértékük ->sum , kiiras a képre
+output=DrawCircles(output,circles)  #a kimeneti képen megrajzolja a megtalált köröket
+
+plt.imshow(output) 
 plt.show()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
